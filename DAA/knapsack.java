49556eb49cbaf1e84cf9b1d2fa13062a09dd2094
VatsalS31 <120210435+VatsalS31@users.noreply.github.com>
2022-12-17 00:24:51 +0530

    Update knapsack.java

--- DAA/knapsack.java
+++ DAA/knapsack.java
@@ -3,23 +3,38 @@ public class knapsack
 {
     public static void main(String[] arg) {
     Scanner s = new Scanner(System.in);
+    System.out.println(("enter size:\n"));
+    
     int n=s.nextInt();
+    System.out.println("enter capacity:\n");
     int m=s.nextInt();
+    int total=0;
+
         int p[]=new int[n];
         int w[]=new int[n];
+
+        System.out.println("enter the profit\n");
         for(int i=0;i<n;i++)
         {
+            
             p[i]=s.nextInt();
         }
+        System.out.println("enter the weight\n");
         for(int i=0;i<n;i++)
         {
+           
             w[i]=s.nextInt();
         }
        
         int cost[]=new int[n];
+        System.out.println("cost:");
         for(int i=0;i<n;i++)
         {
             cost[i]=p[i]/w[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(cost[i]);
         }
          class decend_sort
         {
@@ -38,11 +53,33 @@ public class knapsack
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
-            }
+                }
 
-        }
+             }
 
     }
+    for(int i=0;i<n;i++)
+    {
+        if(m>0 && w[i]<=m)
+        {
+            m=m-w[i];
+            total=total+p[i];
+
+
+        }
+        else
+        break;
+        if(m>0)
+    {
+        total=total+p[i]*(m/w[i]);
+    }
+
+    }
+    
+    System.out.println("total profit:\n");
+    System.out.println(total);
+
+    
     
 
 
